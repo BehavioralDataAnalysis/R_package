3901d93af5936c10be4e24fb49a8918105a82af0
Florent Buisson <florent.buisson.oreilly@maskedmails.com>
2023-01-07 17:45:31 -0600

    Initial commit

---
+++ R/BehavioralDataAnalysis-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' BehavioralDataAnalysis
+#'
+#' Bootstrap and Sampling Functions For Behavioral Data Analysis
+#'
+#' @docType package
+#' @author Florent <florent.buisson.oreilly@maskedmails.com>
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @name BehavioralDataAnalysis
+## usethis namespace: start
+#' @useDynLib BehavioralDataAnalysis, .registration = TRUE
+## usethis namespace: end
+NULL
