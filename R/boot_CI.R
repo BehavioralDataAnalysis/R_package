f41022a2bcb0a21baa7ee5af883f93c41d2ed9c3
Florent Buisson <florent.buisson.oreilly@maskedmails.com>
2023-01-08 09:54:28 -0600

    Adding foreach/doParallel loop

--- R/boot_CI.R
+++ R/boot_CI.R
@@ -5,6 +5,7 @@
 #' @param B number of Bootstrap sampling iterations to run
 #' @param conf.level confidence interval percentage to use
 #' @return a vector with two values, the lower and upper bounds of the confidence interval
+#' @importFrom doParallel, dplyr, foreach
 #' @examples
 #' df1 <- data.frame(
 #' id = 1:6,
@@ -13,8 +14,11 @@
 #' boot_CI(df1, function(df) mean(df$x), B=100)
 #' @export
 
-
 boot_CI <- function(df, fct, B = 100, conf.level = 0.90){
+  # Validating dependencies
+  if (!requireNamespace("doParallel", quietly = TRUE)) {
+    stop("Package \"doParallel\" must be installed to use this function.", call. = FALSE)}
+
   #Validating the inputs
   if(nrow(df) == 0) stop("the data provided is empty")
   if(B <= 0) stop("the value provided for the number of Bootstrap simulations is negative")
@@ -28,11 +32,20 @@ boot_CI <- function(df, fct, B = 100, conf.level = 0.90){
   if(B * (1 - conf.level) / 2 < 1) stop("the number of Bootstrap simulations is too small in relation to the confidence level")
   offset = round(B * (1 - conf.level) / 2)
 
-  # NOTE: towards the end of writing the book, I discovered that sapply generally
-  # gives better performance than the boot library
-  boot_vec <- sapply(1:B, function(x){
-    fct(dplyr::slice_sample(df, n = nrow(df), replace = TRUE))
-    })
+  # Running the Bootstrap loop with foreach, and doParallel as backend
+  doParallel::registerDoParallel()
+  boot_vec <- rep(NA, B)
+  inner_df <- df
+
+  boot_vec <- foreach::foreach(i=1:B,
+                 .combine='c',
+                 .packages = "dplyr",
+                 .inorder = FALSE) %dopar% {
+                   sample_df <- dplyr::slice_sample(inner_df, n = nrow(inner_df), replace = TRUE)
+                   fct(sample_df)
+                 }
+  doParallel::stopImplicitCluster()
+
   if(any(is.na(boot_vec))) stop("the function returned an NA")
   boot_vec <- sort(boot_vec, decreasing = FALSE)
 
