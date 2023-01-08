6f5e7af4d96a1670e75ea3c99cec5d2391760038
Florent Buisson <florent.buisson.oreilly@maskedmails.com>
2023-01-07 19:14:55 -0600

    Updating documentation

--- R/BehavioralDataAnalysis-package.R
+++ R/BehavioralDataAnalysis-package.R
@@ -1,15 +1,14 @@
-#' @keywords internal
-"_PACKAGE"
-
-#' BehavioralDataAnalysis
+#' @name BehavioralDataAnalysis
 #'
-#' Bootstrap and Sampling Functions For Behavioral Data Analysis
-#'
+#' @description Bootstrap and Sampling Functions For Behavioral Data Analysis
 #' @docType package
 #' @author Florent <florent.buisson.oreilly@maskedmails.com>
 #' @import Rcpp
 #' @importFrom Rcpp evalCpp
-#' @name BehavioralDataAnalysis
+#' @keywords internal
+"_PACKAGE"
+
+
 ## usethis namespace: start
 #' @useDynLib BehavioralDataAnalysis, .registration = TRUE
 ## usethis namespace: end
